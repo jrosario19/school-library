349ccf4a0249de64a099648069c5071fcc67a8b6
Juan Rosario <jrosario19@hotmail.com>
2022-07-18 17:22:14 -0400

    Add files for person, student and teacher claess

---
+++ student.rb
@@ -0,0 +1,13 @@
+class Student < Person
+   
+    def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+      super(age, name, parent_permission)
+      @classroom = classroom
+    end
+  
+   
+    def play_hookey
+      "¯\(ツ)/¯"
+    end
+  
+end
