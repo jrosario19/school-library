e579886e21339ee17963a8b0b5daaf4adc7b101c
Juan Rosario <jrosario19@hotmail.com>
2022-07-18 17:29:17 -0400

    Solve some linter errors

--- person.rb
+++ person.rb
@@ -1,10 +1,8 @@
 require 'securerandom'
 
-
 class Person
   attr_accessor :name, :age
   attr_reader :id
-
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = SecureRandom.uuid
@@ -13,15 +11,12 @@ class Person
     @parent_permission = parent_permission
   end
 
-
   def can_use_services?
     is_of_age? || @parent_permission
   end
 
-  
   private
 
-  
   def of_age?
     @age >= 18
   end
