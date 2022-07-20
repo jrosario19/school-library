283dd0c529027c6ecbe118593f5f7976e034fdb9
Juan Rosario <jrosario19@hotmail.com>
2022-07-20 12:15:54 -0400

    Refactor student and teacher class

--- student.rb
+++ student.rb
@@ -1,9 +1,9 @@
 require_relative './person'
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom&.students&.push(self)
+    @rentals = []
   end
 
   def play_hookey
